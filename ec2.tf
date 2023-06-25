73d11c761aa33dedc05c7a767a1333e22b710191
emidinho <113139559+emidinho@users.noreply.github.com>
2023-06-26 04:07:55 +0800

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 # Below is the resource block which creates EC2 Instance
 resource "aws_instance" "test" {
-  ami           = "ami-09988af04120b3591"
+  ami           = "ami-090e0fc566929d98b"
   instance_type = "t2.micro"
   tags = {
     Name = "test"
