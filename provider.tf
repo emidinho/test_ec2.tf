fb8272911f357138c08d379ab565b706a47b68a0
emidinho <113139559+emidinho@users.noreply.github.com>
2023-06-26 02:14:24 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
 # Below is the provider which helps in connecting with AWS Account
 provider "aws" {
   region = "us-east-1"
-  shared_credentials_files  = ["C:/Users/ALEMNJU EMIDIO/.aws/credentials"]
   profile = "default"
 }
