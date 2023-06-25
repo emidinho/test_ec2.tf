f31a165a524227a3705462dfc99892ab078dbd85
emidinho <you@example.com>
2023-06-26 06:01:08 +0800

    update

--- user_data.sh
+++ user_data.sh
@@ -4,4 +4,6 @@ yum update -y
 yum install -y httpd.x86_64
 systemctl start httpd.service
 systemctl enable httpd.service
-echo "hello world from ec2 instance $(hostname -f) in $EC2_AVAIL_ZONE" >> /var/www/html/index.html
+echo "<h1> hello world from ec2 instance $(hostname -f) in </h1>" >> /var/www/html/index.html
+echo "<h2> Availability Zone: $availability_zone </h2>" >> /var/www/html/index.html
+echo "<h3> Region: $region </h3>" >> /var/www/html/index.html
