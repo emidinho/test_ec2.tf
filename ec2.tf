8ec024a93f3c9effa05ddc319de73f0b170d8865
emidinho <113139559+emidinho@users.noreply.github.com>
2023-06-26 04:23:07 +0800

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "test" {
   ami           = "ami-090e0fc566929d98b"
   instance_type = "t2.micro"
-  user_data  = "${file("user_data.sh")}"
+  
   tags = {
     Name = "test"
   }
